0b71d28ac21b4fdd78370510f84ad12dfcd5d935
Mike.Marquardt <m.marquardt@boomify.de>
2021-07-08 09:45:48 +0200

    Add custom log-channel with separate logfile | LAR-11

--- config/logging.php
+++ config/logging.php
@@ -100,6 +100,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'main' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/main.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
     ],
 
 ];
